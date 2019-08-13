bda6469d698b825c437b7dee3316d07a32df9116
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-13 19:36:40 +0800

    Implement bubble sort

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -3,18 +3,29 @@ package com.progrema;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] arr = {20, 35, -15, 7, 55, 1, -22};
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
         show("unsorted", arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        bubbleSort(arr);
         show("sorted  ", arr);
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int j = arr.length - 1; j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
     }
 
     private static void selectionSort(int[] arr) {
         int largestIndex;
         for (int stopIndex = arr.length - 1; stopIndex >= 0; stopIndex--) {
             largestIndex = 0;
-            for (int i = 1; i <= stopIndex; i++ ) {
-                if(arr[largestIndex] < arr[i]) {
+            for (int i = 1; i <= stopIndex; i++) {
+                if (arr[largestIndex] < arr[i]) {
                     largestIndex = i;
                 }
             }
