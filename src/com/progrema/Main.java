390768aa3c6fc8eea03f53369476e94f8ba1a831
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-13 19:02:54 +0800

    Add swap function

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -4,12 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] arr = {20, 35, -15, 7, 55, 1, -22};
-        bubbleSort(arr);
+        show("unsorted", arr);
+        swap(arr, 0, 1);
+        show("sorted  ", arr);
     }
 
-    public static void bubbleSort(int[] arr) {
+    private static void show(String name, int[] arr) {
+        System.out.print(name + ": ");
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
