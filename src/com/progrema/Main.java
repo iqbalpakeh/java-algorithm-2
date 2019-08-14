27e2ad5e7d0e24224c064bd4b18d0f74efe58740
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-15 06:33:18 +0700

    Code refactor

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -9,28 +9,29 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arrInit = {20, 35, -15, 7, 55, 1, -22};
-        int[] arr;
+        // int[] arrInit = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arr = new int[arrInit.length];
 
         System.out.println("\nBubble Sort");
-        arr = arrInit;
+        copyArr(arrInit, arr);
         show("unsorted", arr);
         bubbleSort(arr);
         show("sorted  ", arr);
 
         System.out.println("\nSelection Sort");
-        arr = arrInit;
+        copyArr(arrInit, arr);
         show("unsorted", arr);
         selectionSort(arr);
         show("sorted  ", arr);
 
         System.out.println("\nInsertion Sort");
-        arr = arrInit;
+        copyArr(arrInit, arr);
         show("unsorted", arr);
         insertionSort(arr);
         show("sorted  ", arr);
 
         System.out.println("\nShell Sort");
-        arr = arrInit;
+        copyArr(arrInit, arr);
         show("unsorted", arr);
         shellSort(arr);
         show("sorted  ", arr);
@@ -43,6 +44,12 @@ public class Main {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+
+    private static void copyArr(int[] sourceArr, int[] destinationArr) {
+        for (int i = 0; i < sourceArr.length; i++) {
+            destinationArr[i] = sourceArr[i];
+        }
     }
 
 }
