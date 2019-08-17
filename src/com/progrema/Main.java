4c884771b9a075886393d30fc528864a220e4857
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-17 23:10:54 +0700

    Activate all call on main file

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -2,8 +2,7 @@ package com.progrema;
 
 import com.sun.istack.internal.NotNull;
 
-import static com.progrema.Sort.countingSort;
-import static com.progrema.Sort.quickSort;
+import static com.progrema.Sort.*;
 
 public class Main {
 
@@ -13,57 +12,47 @@ public class Main {
         // int[] arrInit = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] arr = new int[arrInit.length];
 
-        // System.out.println("\nBubble Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // bubbleSort(arr);
-        // show("sorted  ", arr);
-        //
-        // System.out.println("\nSelection Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // selectionSort(arr);
-        // show("sorted  ", arr);
-        //
-        // System.out.println("\nInsertion Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // insertionSort(arr);
-        // show("sorted  ", arr);
-        //
-        // System.out.println("\nShell Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // shellSort(arr);
-        // show("sorted  ", arr);
-        //
-        // System.out.println("\nMerge Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // mergeSort(arr);
-        // show("sorted  ", arr);
-        //
-        // System.out.println("\nQuick Sort");
-        // copyArr(arrInit, arr);
-        // show("unsorted", arr);
-        // quickSort(arr);
-        // show("sorted  ", arr);
+        System.out.println("\nBubble Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        bubbleSort(arr);
+        show("sorted  ", arr);
 
-        {
-            int[] arrCount = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
-            System.out.println("\nCounting Sort");
-            show("unsorted", arrCount);
-            countingSort(arrCount);
-            show("sorted  ", arrCount);
-        }
+        System.out.println("\nSelection Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        selectionSort(arr);
+        show("sorted  ", arr);
 
-        {
-            int[] arrCount = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
-            System.out.println("\nQuick Sort");
-            show("unsorted", arrCount);
-            quickSort(arrCount);
-            show("sorted  ", arrCount);
-        }
+        System.out.println("\nInsertion Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        insertionSort(arr);
+        show("sorted  ", arr);
+
+        System.out.println("\nShell Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        shellSort(arr);
+        show("sorted  ", arr);
+
+        System.out.println("\nMerge Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        mergeSort(arr);
+        show("sorted  ", arr);
+
+        System.out.println("\nQuick Sort");
+        copyArr(arrInit, arr);
+        show("unsorted", arr);
+        quickSort(arr);
+        show("sorted  ", arr);
+
+        int[] arrCount = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+        System.out.println("\nCounting Sort");
+        show("unsorted", arrCount);
+        countingSort(arrCount);
+        show("sorted  ", arrCount);
 
     }
 
