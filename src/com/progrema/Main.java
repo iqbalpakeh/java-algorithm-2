0f48795d910286f2fbb5c21f602b08101a376630
Progrema Studio <iqbalpakeh@gmail.com>
2019-08-21 03:52:43 +0700

    Code refactor

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -1,13 +1,14 @@
 package com.progrema;
 
 import com.progrema.linkedlist.DoublyLinkedList;
+import com.progrema.sort.Sort;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Sort.test();
         DoublyLinkedList.test();
-
     }
 
 }
--- src/com/progrema/sort/Sort.java
+++ src/com/progrema/sort/Sort.java
@@ -1,4 +1,4 @@
-package com.progrema;
+package com.progrema.sort;
 
 import com.sun.istack.internal.NotNull;
 
