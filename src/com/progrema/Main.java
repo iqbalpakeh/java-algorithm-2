b2e6a636f2a6961490460d49c42201a3c4c19261
Progrema Studio <iqbalpakeh@gmail.com>
2019-10-05 03:24:45 +0700

    Implement test palindrome with stack and queue

--- src/com/progrema/Main.java
+++ src/com/progrema/Main.java
@@ -1,6 +1,9 @@
 package com.progrema;
 
 import com.progrema.queue.CircularArrayQueue;
+import com.progrema.queue.Queue;
+import com.progrema.stack.LinkedListStack;
+import com.progrema.stack.Stack;
 
 public class Main {
 
@@ -17,7 +20,45 @@ public class Main {
 
         // ArrayQueue.test();
         // LinkedListQueue.test();
-        CircularArrayQueue.test();
+        // CircularArrayQueue.test();
+
+        testPalindrome();
+    }
+
+    public static void testPalindrome() {
+        {
+            char[] word = "racecar".toCharArray();
+            System.out.println("check " + String.valueOf(word) + " : " + isPalindrome(word));
+        }
+
+        {
+            char[] word = "hello".toCharArray();
+            System.out.println("check " + String.valueOf(word) + " : " + isPalindrome(word));
+        }
+
+        {
+            char[] word = "ididdidi".toCharArray();
+            System.out.println("check " + String.valueOf(word) + " : " + isPalindrome(word));
+        }
+    }
+
+    private static boolean isPalindrome(char[] word) {
+
+        Queue<Character> queue = new CircularArrayQueue<>();
+        Stack<Character> stack = new LinkedListStack<>();
+
+        for (int i = 0; i < word.length; i++) {
+            queue.enqueue(word[i]);
+            stack.push(word[i]);
+        }
+
+        for (int i = 0; i < word.length; i++) {
+            if (queue.dequeue() != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
