b61cc1cbfdd4f92dcd653c0b6132f882098bfe90
Progrema Studio <iqbalpakeh@gmail.com>
2019-10-23 18:26:54 +0700

    Implement linear probing

--- src/com/progrema/hashtable/LinearProbing.java
+++ src/com/progrema/hashtable/LinearProbing.java
@@ -4,26 +4,28 @@ public class LinearProbing<K, V> implements HashTable<K, V> {
 
     // ------------------------- TEST -------------------------
 
+    private int INIT_SIZE = 20;
+
+    // ------------------------- IMPLEMENTATION -------------------------
+    private int count;
+
+    private int size = INIT_SIZE;
+    private V[] arr;
+
     public static void test() {
 
         HashTable<Integer, String> ht = new LinearProbing<>();
 
-        ht.put(1, "One");
-        ht.put(2, "Two");
+        // ht.put(1, "One");
+        // ht.put(2, "Two");
         ht.put(3, "Three");
         ht.put(4, "Four");
-        ht.put(5, "Five");
+        // ht.put(5, "Five");
+        ht.put(23, "TwentyThree"); // collision with 3
 
         System.out.print(ht.get(5));
 
     }
-
-    // ------------------------- IMPLEMENTATION -------------------------
-
-    private int INIT_SIZE = 5;
-
-    private int size = INIT_SIZE;
-    private V[] arr;
 
     public LinearProbing() {
         arr = (V[]) new Object[size];
@@ -31,12 +33,28 @@ public class LinearProbing<K, V> implements HashTable<K, V> {
 
     @Override
     public void put(K key, V value) {
-        arr[hash(key)] = value;
+
+        // resize hash table if necessary
+        if (count > size / 2) {
+            arr = resize(2 * size);
+        }
+
+        // probe index if collision happened
+        int index = hash(key);
+        while (arr[index] != null) {
+            index = (index + 1) % size;
+        }
+
+        // store value and increase counter
+        arr[index] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
-        return arr[hash(key)];
+        int index = hash(key);
+
+        return arr[index];
     }
 
     @Override
@@ -46,6 +64,26 @@ public class LinearProbing<K, V> implements HashTable<K, V> {
 
     private int hash(K key) {
         return (key.hashCode() & 0x7fffffff) % size;
+    }
+
+    private V[] resize(int size) {
+        V[] newArr = (V[]) new Object[size];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+
+    private static class Pair<K, V> {
+
+        private K k;
+        private V v;
+
+        public Pair(K key, V value) {
+            k = key;
+            v = value;
+        }
+
     }
 
 }
